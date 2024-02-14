7a9d178f9d657a7710d531a8fd413fa9ec27e7bb
mahedi066 <imam25349@gmail.com>
2024-02-14 16:08:27 +0600

    love calculator application

---
+++ index.js
@@ -0,0 +1,15 @@
+function clickMe(){
+    const getInput=document.getElementById('first-inp');
+    const getInput2=document.getElementById('secound-inp');
+    const name=getInput.value+' '+"Love"+" " +getInput2.value;
+
+    const randomNumber=Math.round(Math.random()*100);
+    const ouput=document.getElementById('outpu');
+    ouput.innerText=name+" " +randomNumber+" "+'%'
+    getInput.value='';
+    getInput2.value='';
+   
+
+   
+   
+}
